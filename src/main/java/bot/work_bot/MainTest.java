530b3ac0a99a22b4bfa3b8bc0f926015dd6d55c5
Ruslan Krachoon <lord.vader.1997@gmail.com>
2020-05-16 15:04:37 +0300

    Check

---
+++ src/main/java/bot/work_bot/MainTest.java
@@ -0,0 +1,4 @@
+package bot.work_bot;
+
+public class MainTest {
+}
--- src/main/java/bot/work_bot/WorkBotApplication.java
+++ src/main/java/bot/work_bot/WorkBotApplication.java
@@ -8,7 +8,6 @@ public class WorkBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkBotApplication.class, args);
-        System.out.println();
-    }
 
+    }
 }
