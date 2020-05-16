43266416882b0a2010eb5cebc79beab3f417eaa0
Ruslan Krachoon <lord.vader.1997@gmail.com>
2020-05-16 15:01:53 +0300

    CHekc

---
+++ src/main/java/bot/work_bot/WorkBotApplication.java
@@ -0,0 +1,14 @@
+package bot.work_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkBotApplication.class, args);
+        System.out.println();
+    }
+
+}
---
+++ src/test/java/bot/work_bot/WorkBotApplicationTests.java
@@ -0,0 +1,13 @@
+package bot.work_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
